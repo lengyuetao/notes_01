5d88503dad774f0a6dea7f3af088763f0536f34e
dyadan@163.com <dyadan@163.com>
2018-11-25 17:36:54 +0800

    添加demo类

---
+++ src/main/java/com/example/notes/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.example.notes.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginController extends HttpServlet {
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getHeader("");
+        req.getMethod();
+        req.getSession();
+        super.service(req, resp);
+    }
+}
